b474477fd3170c087ce5dd2768d2559c05823804
iota07 <hanesananhouvannasak@googlemail.com>
2023-11-14 17:09:59 +0100

    add array ex

---
+++ array/array.php
@@ -0,0 +1,11 @@
+<?php
+    $countries = array( 'Belgium', 'France' , 'Germany', 'Netherlands', 'Ukraine' ); 
+    echo $countries[2]."<br><br>";
+
+    $family_member = ["Toumkham","Thavixay","Chantaphone","Amphayvone","Sayaphoum","Varouny"];
+    print_r($family_member);
+    echo '<br>';
+    $favourite_recipe = ["Katsudon","Curry rice","Wonton","Okonomiyaki"];
+    print_r($favourite_recipe);
+
+?>
