d07afde749ef5c9114c184f9aabce79cae31fde2
iota07 <hanesananhouvannasak@googlemail.com>
2023-11-14 15:14:18 +0100

    changing to concatenated p tag

--- variable/variable_type.php
+++ variable/variable_type.php
@@ -1,21 +1,16 @@
 <?php 
     $name = 'Han'; 
-        echo "<p>Hi my name is {$name} </p>";
+        echo '<p>'."Hi my name is {$name}". '</p>';
     $age = '40';
-        echo "<p>I am {$age} old.</p>";
+        echo '<p>'."I am {$age} old.".'</p>';
     $eyecolor = 'dark brown';
-        echo "<p>My eyes are {$eyecolor}.</p>";
+        echo '<p>'."My eyes are {$eyecolor}". '</p>';
     $familymember = array("Toumkham","Thavixay","Chantaphone","Amphayvone","Sayaphoum","Varouny");
 
         foreach($familymember as $familymember){
-            echo $familymember . "<br>";
+            echo $familymember . '<br>';
         }
     $hungry = true;
-        echo "<p>am I hungry? 1 = true, 0 = false: {$hungry}</p>"
-
-
-
-
-
+        echo '<p>'." 1 = true, 0 = false / Am I hungry?: {$hungry}".'</p>'
 
 ?>
